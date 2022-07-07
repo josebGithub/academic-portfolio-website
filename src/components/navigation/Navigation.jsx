11fbb6c0c9ef0d8940c846a95036641eca4cf286
josebGithub <46608139+josebGithub@users.noreply.github.com>
2022-07-07 16:16:47 -0700

    Created the fixed position menu bar

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -5,15 +5,17 @@ import { TiUser} from 'react-icons/ti'
 import {MdWork} from 'react-icons/md'
 import {AiOutlineFileSearch} from 'react-icons/ai'
 import {BiMessageDetail} from 'react-icons/bi'
+import{useState} from 'react'
 
 const Navigation = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><TiHomeOutline/></a>
-      <a href="#about"><TiUser/></a>
-      <a href="#experience"><MdWork/></a>
-      <a href="#portfolio"><AiOutlineFileSearch/></a>
-      <a href="#contact"><BiMessageDetail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} classname={activeNav === '#' ? 'active' : ''}><TiHomeOutline/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} classname={activeNav === '#about' ? 'active' : ''}><TiUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} classname={activeNav === '#experience' ? 'active' : ''}><MdWork/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} classname={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFileSearch/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} classname={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
     </nav>
   )
 }
